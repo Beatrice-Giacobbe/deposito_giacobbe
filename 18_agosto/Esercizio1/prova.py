deebce38682069e92a013b256fb988b63f93e86c
Beatrice-Giacobbe <beatrice.giacobbe@it.ey.com>
2025-08-18 14:42:46 +0200

    Create prova.py

---
+++ 18_agosto/Esercizio1/prova.py
@@ -0,0 +1,9 @@
+from collections import Counter
+file = "C:\\Users\\SV273YL\\OneDrive - EY\\Documents\\GitHub\\deposito_giacobbe\\18_agosto\\Esercizio1\\input.txt"
+
+with open(file, "r", encoding="utf-8") as f:
+        contenuto = f.read()
+#print(contenuto)
+def conta_righe(testo):
+return len(testo.split("\n"))
+print("il testo contiene", conta_righe(contenuto), "righe")
