8d3f5177aec08a7c2638564d7c956332dfbe5250
Beatrice-Giacobbe <beatrice.giacobbe@it.ey.com>
2025-08-19 11:19:29 +0200

    refactor and add a feat

--- 19_agosto/Esercizio1/eser1.py
+++ 19_agosto/Esercizio1/eser1.py
@@ -3,27 +3,26 @@ import pandas as pd
 path = r"C:\Users\SV273YL\OneDrive - EY\Desktop\datasetConsumption\AEP_hourly.csv"
 df = pd.read_csv(path, parse_dates=["Datetime"])
 
-df["Data"] = df["Datetime"].dt.date
 media = df['AEP_MW'].mean()
-print("val medio", media)
+#print("val medio", media)
 
-# rispetto a valore globale
-def classifica_globale(el):
+def classifica(el,media):
     if el["AEP_MW"] > media:
         return "Alto"
     else:
         return "Basso"
-df["target_globale"] = df.apply(classifica_globale, axis=1)
+    
+# rispetto a valore globale
+df["target_giornaliero"] = df.apply(lambda row: classifica(row, media), axis=1)
 
 #rispetto a media del giorno settimanale
 df["Giorno_settimana"] = df["Datetime"].dt.dayofweek
-df["media_giornaliera"] = df.groupby("Giorno_settimana")["AEP_MW"].transform("mean")
-def classifica_giornaliera(el):
-    if el["AEP_MW"] > el["media_giornaliera"]:
-        return "Alto"
-    else:
-        return "Basso"
+df["media_giornaliera_set"] = df.groupby("Giorno_settimana")["AEP_MW"].transform("mean")
+df["target_giornaliero_sett"] = df.apply(lambda row: classifica(row, row["media_giornaliera_set"]), axis=1)
 
-df["target_giornaliero"] = df.apply(classifica_giornaliera, axis=1)
+#rispetto al giorno
+df['Giorno'] = df["Datetime"].dt.day
+df["media_giornaliera"] = df.groupby("Giorno")["AEP_MW"].transform("mean")
+df["target_giornaliero"] = df.apply(lambda row: classifica(row, row["media_giornaliera"]), axis=1)
 
-print(df)
+print(df.head(5))
