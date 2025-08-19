ebe587cc725e90db7da9a3b62214ee978856e7f7
Beatrice-Giacobbe <beatrice.giacobbe@it.ey.com>
2025-08-19 11:37:35 +0200

    add decision tree and some fix

--- 19_agosto/Esercizio1/eser1.py
+++ 19_agosto/Esercizio1/eser1.py
@@ -1,5 +1,8 @@
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
 path = r"C:\Users\SV273YL\OneDrive - EY\Desktop\datasetConsumption\AEP_hourly.csv"
 df = pd.read_csv(path, parse_dates=["Datetime"])
 
@@ -8,9 +11,9 @@ media = df['AEP_MW'].mean()
 
 def classifica(el,media):
     if el["AEP_MW"] > media:
-        return "Alto"
+        return 1
     else:
-        return "Basso"
+        return 0
     
 # rispetto a valore globale
 df["target_giornaliero"] = df.apply(lambda row: classifica(row, media), axis=1)
@@ -26,3 +29,23 @@ df["media_giornaliera"] = df.groupby("Giorno")["AEP_MW"].transform("mean")
 df["target_giornaliero"] = df.apply(lambda row: classifica(row, row["media_giornaliera"]), axis=1)
 
 print(df.head(5))
+
+#con decision tree
+x = df[['Giorno', 'Giorno_settimana']]
+y = df['target_giornaliero']
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, stratify=y, test_size=0.25, random_state=42
+)
+
+tree = DecisionTreeClassifier(max_depth=5, random_state=42)
+tree.fit(X_train, y_train)
+y_pred = tree.predict(X_test)
+
+df1 = pd.DataFrame({
+    "X_test": list(X_test.to_records(index=False)),  # converte le righe in tuple
+    "Pred": y_pred
+})
+print(df1.head())
+
+#df1 = pd.DataFrame({"X_test": X_test,"Pred": y_pred})
+print(df1)
