484a936c2306d5257ec23644e2ad962da36c5139
Beatrice-Giacobbe <beatrice.giacobbe@it.ey.com>
2025-08-19 11:08:40 +0200

    implentazione intero eser

---
+++ 19_agosto/Esercizio1/eser1.py
@@ -0,0 +1,25 @@
+
+df["Data"] = df["Datetime"].dt.date
+media = df['AEP_MW'].mean()
+print("val medio", media)
+
+# rispetto a valore globale
+def classifica_globale(el):
+    if el["AEP_MW"] > media:
+        return "Alto"
+    else:
+        return "Basso"
+df["target_globale"] = df.apply(classifica_globale, axis=1)
+
+#rispetto a media del giorno settimanale
+df["Giorno_settimana"] = df["Datetime"].dt.dayofweek
+df["media_giornaliera"] = df.groupby("Giorno_settimana")["AEP_MW"].transform("mean")
+def classifica_giornaliera(el):
+    if el["AEP_MW"] > el["media_giornaliera"]:
+        return "Alto"
+    else:
+        return "Basso"
+
+df["target_giornaliero"] = df.apply(classifica_giornaliera, axis=1)
+
+print(df)
